182c349348431c8b8c216873c7607c2bb944d5f1
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-11-15 22:25:28 +0200

    Api done

--- Bugred/RegistrationRequestModel.cs
+++ Bugred/RegistrationRequestModel.cs
@@ -12,13 +12,17 @@ namespace Bugred
         [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
         [JsonProperty("password")]
         public string Password { get; set; }
 
-        [JsonProperty("createcompany")]
-        public string CreateCompany { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        public API(string email, string name, string password)
+        {
+            this.Email = email;
+            this.Name = name;
+            this.Password = password;
+        }
     }
 }
