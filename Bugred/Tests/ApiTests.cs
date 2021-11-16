a9ed388171abaad90a6eb98d092364730fdd0726
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-11-16 09:49:52 +0200

    commit

--- Bugred/Tests/ApiTests.cs
+++ Bugred/Tests/ApiTests.cs
@@ -14,7 +14,29 @@ namespace Bugred
         {
             Helper helper = new Helper();
             
-            API body = new API(helper.name); //name? email? password
+           // API body = new API(Helper.RandomStringGeneration); 
+            RequestHelper requestHelper = new RequestHelper("tasks/rest/doregister");
+            //RegistrationRequestModel body = new RegistrationRequestModel();
+            //IRestResponse response = requestHelper.SendPostRequest(body);
+            // JObject json = JObject.Parse(response.Content);
+            API body = new API()
+            {
+                Email = "arkadii" + helper.RandomStringGeneration + "@mail.ru",
+                Name = " kololo",
+                Password = "1"
+
+            };
+            Assert.AreEqual("OK", response.StatusCode.ToString());
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(body["email"], json["account"]["email"]?.ToString());
+        }
+
+        [Test]
+        public void Registration()
+        {
+            Helper helper = new Helper();
+
+            API body = new API(Helper.RandomStringGeneration);
             RequestHelper requestHelper = new RequestHelper("/doregister");
             IRestResponse response = requestHelper.SendPostRequest(body);
             JObject json = JObject.Parse(response.Content);
